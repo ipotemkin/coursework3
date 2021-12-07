8ccaf9b1c7070c0cd45436821e78b7f9f55eb03b
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-12-07 23:49:57 +0300

    user model: added columns email, name, surname

--- app/dao/model/users.py
+++ app/dao/model/users.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from sqlalchemy import Column, Integer, String
 from app.dao.model.base import Base
@@ -7,9 +7,12 @@ from app.dao.model.base import Base
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String)
-    role = Column(String)
+    username = Column(String, unique=True, nullable=False)  # to be deprecated for email
+    password = Column(String)  # a hashed password
+    role = Column(String)  # to be deprecated
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    surname = Column(String(100), nullable=False)
 
     def __repr__(self):
         return f"<User: id={self.id}, username={self.username}, role={self.role}>"
@@ -19,6 +22,9 @@ class UserBase(BaseModel):
     username: str
     password: Optional[str]
     role: str
+    email: EmailStr
+    name: str
+    surname: str
 
     class Config:
         orm_mode = True
@@ -32,3 +38,6 @@ class UserUpdateBM(UserBase):
     username: Optional[str]
     password: Optional[str]
     role: Optional[str]
+    email: Optional[EmailStr]
+    name: Optional[str]
+    surname: Optional[str]
