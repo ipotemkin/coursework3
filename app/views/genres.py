3bba5e39da11dd0a6ae6dd21ba63c46028b17cce
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-12-11 00:25:19 +0300

    cleaned app.views.genres

--- app/views/genres.py
+++ app/views/genres.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, status, Response, Depends
-from app.dao.model.genres import GenreBM, GenreUpdateBM
+from fastapi import APIRouter, Depends
+# from app.dao.model.genres import GenreBM, GenreUpdateBM
 from app.service.genres import GenreService
 from app.dependencies import get_db
 from sqlalchemy.orm import Session
@@ -10,15 +10,11 @@ router = APIRouter(prefix='/genres', tags=['genres'])
 
 @router.get('', summary='Получить все жанры')
 @router.get('/', summary='Получить все жанры', include_in_schema=False)
-async def genres_get_all(page: int = None, db: Session = Depends(get_db),
-                         # decoded_token=Depends(valid_token)
-                         ):
+async def genres_get_all(page: int = None, db: Session = Depends(get_db)):
     """
     Получить все жанры
     """
     return GenreService(db).get_all(page=page)
-    # genre_id = UserService(db).get_all_by_filter({'email': decoded_token.get('email')})[0].get('favorite_genre')
-    # return GenreService(db).get_one(genre_id)
 
 
 @router.get('/{pk}', summary='Получить жанр по его ID')
@@ -31,36 +27,36 @@ async def genres_get_one(pk: int, db: Session = Depends(get_db)):
     return GenreService(db).get_one(pk)
 
 
-@router.post('', status_code=status.HTTP_201_CREATED, summary='Добавить жанр',
-             response_description="The created item")
-async def genres_post(genre: GenreBM, response: Response, db: Session = Depends(get_db)):
-    """
-    Добавить жанр:
-
-    - **id**: ID жанра - целое число (необязательный параметр)
-    - **name**: название жанра (обязательный параметр)
-    """
-    new_obj = GenreService(db).create(genre.dict())
-    response.headers['Location'] = f'{router.prefix}/{new_obj.id}'
-    return new_obj
-
-
-@router.patch('/{pk}',
-              # status_code=status.HTTP_204_NO_CONTENT,
-              summary='Изменить запись о жанре с указанным ID')
-async def genres_update(genre: GenreUpdateBM, pk: int, db: Session = Depends(get_db)):
-    """
-    Изменить запись о жанре с указанным ID:
-
-    - **name**: изменить название жанра
-    """
-    return GenreService(db).update(genre.dict(), pk)
-
-
-@router.delete('/{pk}', status_code=status.HTTP_200_OK, summary='Удалить запись о жанре с указанным ID')
-async def genres_delete(pk: int, db: Session = Depends(get_db)):
-    """
-    Удалить запись о жанре с указанным ID:
-    """
-    GenreService(db).delete(pk)
-    # return None
+# @router.post('', status_code=status.HTTP_201_CREATED, summary='Добавить жанр',
+#              response_description="The created item")
+# async def genres_post(genre: GenreBM, response: Response, db: Session = Depends(get_db)):
+#     """
+#     Добавить жанр:
+#
+#     - **id**: ID жанра - целое число (необязательный параметр)
+#     - **name**: название жанра (обязательный параметр)
+#     """
+#     new_obj = GenreService(db).create(genre.dict())
+#     response.headers['Location'] = f'{router.prefix}/{new_obj.id}'
+#     return new_obj
+#
+#
+# @router.patch('/{pk}',
+#               # status_code=status.HTTP_204_NO_CONTENT,
+#               summary='Изменить запись о жанре с указанным ID')
+# async def genres_update(genre: GenreUpdateBM, pk: int, db: Session = Depends(get_db)):
+#     """
+#     Изменить запись о жанре с указанным ID:
+#
+#     - **name**: изменить название жанра
+#     """
+#     return GenreService(db).update(genre.dict(), pk)
+#
+#
+# @router.delete('/{pk}', status_code=status.HTTP_200_OK, summary='Удалить запись о жанре с указанным ID')
+# async def genres_delete(pk: int, db: Session = Depends(get_db)):
+#     """
+#     Удалить запись о жанре с указанным ID:
+#     """
+#     GenreService(db).delete(pk)
+#     # return None
