859463018d1b3d983f4f8769f721832252ba0cca
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-12-17 22:56:28 +0300

    added some tests and .env.template

--- app/constants.py
+++ app/constants.py
@@ -3,9 +3,6 @@ from app.config import settings
 JWT_KEY = settings.jwt_key
 PWD_HASH_SALT = settings.pwd_hash_salt
 
-# PWD_HASH_SALT = b"the highest secret possible"
-# JWT_KEY = "SkyPro2021!"
-
 PWD_HASH_ITERATIONS = 100_000
 AC_TOKEN_EXP_TIME_MIN = 30
 R_TOKEN_EXP_TIME_DAYS = 1
